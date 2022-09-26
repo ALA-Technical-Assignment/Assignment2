80e837eb82e2e25bd4aa471c5f2c659ed12d1804
JKAmbe <89562604+JKAmbe@users.noreply.github.com>
2022-09-26 19:43:01 +1000

    Found all the necessary function to complete task, including:
    Get material from object
    Import Material as MB to scene
    Export Material into MB
    Set material into Object
    read and write into JSON

--- surfacing.py
+++ surfacing.py
@@ -1,1 +1,18 @@
+import maya.cmds as cmds
+import json
 
+#find material in object
+def getMaterialFromObject(target):
+    shadeEng = cmds.listConnections(target , type = "shadingEngine")
+    materials = cmds.ls(mc.listConnections(shadeEng ), materials = True)
+    return materials
+
+#export selected material into Maya Binary
+cmds.file("D:/Uni/TD/asd/s.mb",op = "v=0",typ="mayaBinary",pr=True,es=True)
+
+#set material to object
+cmds.sets(e=True, forceElement="standardSurface1SG");
+
+#create and write to json
+with  open('D:/Uni/TD/new_file.json','w') as f:
+    json.dump('Will be inside json',f)
