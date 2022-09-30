4177e433ed7177d168319a392c10c10e409427dd
Hellostar-0001 <99328915+Hellostar-0001@users.noreply.github.com>
2022-10-01 02:02:23 +1000

    Update publish.py

--- publish.py
+++ publish.py
@@ -1,79 +1,120 @@
 import maya.cmds as cmds
 import os
+import pathlib
 
-path = ''
+path = r"{}".format(cmds.file(q=True, sceneName=True))
 prefixPath = ''
 folderPath = ''
 files = []
+fileName = ''
+
+# Methods to execute
+def nameFile(currentValue):
+   global fileName
+   if not files:
+      name = cmds.textFieldGrp(label = 'File Name', editable=True)
+      fileName = name + "v_001"
+   else:
+      stringList = currentValue.split("_")
+      versionArray = [int(i) for i in stringList[-1].split() if i.isdigit()]
+      versionNo = versionArray[0]
+      stringList.pop(-1)
+      updateNo = '_' + f'{versionNo:03d}'
+      updateVersion = '_'.join(stringList) + updateNo
+      fileName = cmds.textFieldGrp(label = 'File Name', editable=True, text=updateVersion)
+
+def createOptionMenu():
+   cmds.optionMenu( label= 'Content', changeCommand=nameFile )
+   if not files:
+      cmds.menuItem(label = '')
+   else:
+      for i in files:
+         cmds.menuItem( label= i )
+
+def joinPath(filePath, newElement):
+   newPath = pathlib.PurePath(filePath, newElement)
+   return newPath
+
+# UI Functions
+
+def confirm():
+    global prefixPath
+    prefixPath = cmds.textFieldGrp(path, q =True, text = True)
+    print(prefixPath)
 
 def openCharater():
-   folderPath = folderPath + "\Asset"
-   if(os.path.isdir(folderPath)):
-      destPath = folderPath + "\Character"
-      if(os.path.isdir(destPath)):
-         files = os.listdir(destPath)
+   global folderPath
+   global files
+   assetPath = joinPath(folderPath,"Asset")
+   folderPath = joinPath(assetPath,"Character")
+   if(os.path.isdir(assetPath)):
+      if(os.path.isdir(folderPath)):
+         files = os.listdir(folderPath)
       else:
-         os.mkdir(destPath)
-         files = os.listdir(destPath)
+         os.mkdir(folderPath)
    else:
+      os.mkdir(assetPath)
       os.mkdir(folderPath)
-      os.chdir(folderPath)
+   createOptionMenu()
 
 def openProp():
-   folderPath = folderPath + "\Asset"
-   if(os.path.isdir(folderPath)):
-      destPath = folderPath + "\Prop"
-      if(os.path.isdir(destPath)):
-         files = os.listdir(destPath)
+   global folderPath
+   global files
+   assetPath = joinPath(folderPath, "Asset")
+   folderPath = joinPath(assetPath, "Prop")
+   if(os.path.isdir(assetPath)):
+      if(os.path.isdir(folderPath)):
+         files = os.listdir(folderPath)
       else:
-         os.mkdir(destPath)
-         files = os.listdir(destPath)
+         os.mkdir(folderPath)
    else:
+      os.mkdir(assetPath)
       os.mkdir(folderPath)
-      os.chdir(folderPath)
+   createOptionMenu()
 
 def openSet():
-   folderPath = folderPath + "\Asset"
-   if(os.path.isdir(folderPath)):
-      destPath = folderPath + "\Set"
-      if(os.path.isdir(destPath)):
-         os.chdir(destPath)
-         files = os.listdir(destPath)
+   global folderPath
+   global files
+   assetPath = joinPath(folderPath, "Asset")
+   folderPath = joinPath(assetPath, "Set")
+   if(os.path.isdir(assetPath)):
+      if(os.path.isdir(folderPath) == False):
+         os.mkdir(folderPath)
       else:
-         os.mkdir(destPath)
-         files = os.listdir(destPath)
+         files = os.listdir(folderPath)
    else:
+      os.mkdir(assetPath)
       os.mkdir(folderPath)
-      os.chdir(folderPath)
+   createOptionMenu()
 
 def openSetPiece():
-   folderPath = folderPath + "\Asset"
-   if(os.path.isdir(folderPath)):
-      destPath = folderPath + "\SetPiece"
-      if(os.path.isdir(destPath)):
-         os.chdir(destPath)
+   global folderPath
+   global files
+   assetPath = joinPath(folderPath, "Asset")
+   folderPath = joinPath(assetPath, "SetPiece")
+   if(os.path.isdir(assetPath)):
+      if(os.path.isdir(folderPath) == False):
+         os.mkdir(folderPath)
       else:
-         os.mkdir(destPath)
-         files = os.listdir(destPath)
+         files = os.listdir(folderPath)
    else:
+      os.mkdir(assetPath)
       os.mkdir(folderPath)
-      os.chdir(folderPath)
+   createOptionMenu()
 
 def openSequence():
-   folderPath = prefixPath + "\Sequence"
-   if(os.path.isdir(folderPath)):
-      os.chdir(folderPath)
-      files = os.listdir(folderPath)
-   else:
+   global folderPath
+   global files
+   folderPath = joinPath(folderPath,"Sequence")
+   if(os.path.isdir(folderPath) == False):
       os.mkdir(folderPath)
-      os.chdir(folderPath)
       files = os.listdir(folderPath)
-
-def nameFile():
-   print("test")
+   createOptionMenu()   
 
 def saveFile():
-   print("test")
+   customPth = joinPath(folderPath, fileName) 
+   cmds.file(rename = customPth)
+   cmds.file(s=True,f=True, typ= "mayaBinary")
 
 def publishFile():
    print("test")
@@ -84,37 +125,46 @@ def saveWindowCancel():
 def publishWindowCancel():
    print("test")
 
-def prefixPath():
-   prefixPath = cmds.textFieldGrp(path, q = True, text = True)
-   if(os.path.isdir(prefixPath)):
-      os.chdir(prefixPath)
-      print(prefixPath)
-   else:
-      print("Warning: Folder Path does not exist! ")
-
 
 def save_window():
-   folderPath = prefixPath + "\wip"
-   if(os.path.isdir(folderPath)):
-      os.chdir(folderPath)
-   else:
+   folderPath = joinPath(prefixPath, "wip")
+   if(os.path.isdir(folderPath) == False):
       os.mkdir(folderPath)
-      os.chdir(folderPath)
    if cmds.window('save_publish_init', exists = True):
       cmds.deleteUI('save_publish_init')
+   if cmds.window('save_window', exists = True):
+      cmds.deleteUI('save_window')
    cmds.window('save_window', resizeToFitChildren=True)
+
    cmds.scrollLayout()
+
+   cmds.separator(h=10)
+   cmds.text('Asset')
+   cmds.separator(h=10)
+
+   cmds.button( label='Character', command='openCharater()')
+   cmds.button( label='Prop', command='openProp()')
+   cmds.button( label='Set', command='openSet()')
+   cmds.button( label='SetPiece', command='openSetPiece()')
+
+   cmds.separator(h=10)
+   cmds.text('Sequence')
+   cmds.separator(h=10)
+
+   cmds.button( label='Sequence', command='openSequence()')
+
+   cmds.separator(h=30)
+
+   cmds.button( label='Save', command='saveFile()')
+
    
    cmds.showWindow('save_window')
 
 
 def publish_window():
-   folderPath = prefixPath + "\publish"
-   if(os.path.isdir(folderPath)):
-      os.chdir(folderPath)
-   else:
+   folderPath = joinPath(prefixPath, "publish")
+   if(os.path.isdir(folderPath) == False):
       os.mkdir(folderPath)
-      os.chdir(folderPath)
    if cmds.window('save_publish_init', exists = True):
       cmds.deleteUI('save_publish_init')
    cmds.window('publish_window', resizeToFitChildren=True)
@@ -129,8 +179,9 @@ def save_publish_init():
    cmds.window('save_publish_init', resizeToFitChildren=True)
    cmds.scrollLayout()
 
-   path = cmds.textFieldGrp(label = 'Folder Path:', text=os.getcwd(),editable=True)
-   cmds.button(l = 'Confirm', command = 'prefixPath()')
+   global path
+   path = cmds.textFieldGrp(label = 'Folder Path:', text=path,editable=True)
+   cmds.button(label = 'Confirm', command = 'confirm()')
    cmds.button(label = 'Save', command = 'save_window()')
    cmds.button(label = 'Publish', command = 'publish_window()')
 
