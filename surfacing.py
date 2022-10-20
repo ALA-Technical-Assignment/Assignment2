c5bb03023b7299537f5bbd734671520d675ac0a3
JKAmbe <89562604+JKAmbe@users.noreply.github.com>
2022-10-21 10:02:43 +1100

    Import functionality halfway done
    Found an issue in the set scene provided, will discuss with tutor before continuing

--- surfacing.py
+++ surfacing.py
@@ -23,6 +23,7 @@ def PublishMaterial():
         print ('Not in WIP')
     #update latestVersionTextBox using MaterialFindlatestVer();
 
+
 def GetMaterialFromObject(target):
     shape = cmds.listRelatives ( target, shapes=True )
     shadingEngine = cmds.listConnections (shape, source=False, destination=True)
@@ -50,6 +51,11 @@ def GetlatestVerInPath(path):
            currentVersion  = ver
     return currentVersion
 
+def GetFileInPathWithVer(path, ver):
+    for fileInPath in os.listdir(path):
+        if str(ver).zfill(3)+'.mb' in fileInPath:
+            return fileInPath     
+
 def GetPublishPath(Name):
     AllParts = pathlib.PurePath(cmds.file(q=True, sn=True)).parts
     if not 'wip' in AllParts:
@@ -62,21 +68,77 @@ def GetPublishPath(Name):
             os.makedirs(os.path.join(*Path+('textures',)))
         return (os.path.join(*Path + ('textures',Name)))
 
-
+#PublishMaterial()
 
 #-------------------------------------------------------------------------------------------------lighting-----------------------------------------------------------------------------------------
 
 
-def setMaterial(object, material):
+#To Do
+    #write failsafe in code
+
+# load all  material
+def LoadAllMaterial():
+        objects = cmds.ls(v = True, typ = 'transform')
+        SurfacingPaths = set()
+        for object in objects:
+            path = getShaderPath(object)
+            #print (path)
+            if (path != None):
+                #print(pathlib.PurePath(cmds.referenceQuery(object ,filename=True)).parts)
+                SurfacingPaths.add(path)
+        for path in SurfacingPaths:
+            print('imported from: \n' + path)
+            cmds.file(path,i=True)
+            accompanyingJson = path [:-3] +'.json'
+            for object in GetContentFromJson(accompanyingJson):
+                cmds.select (object[0])
+            #ask french, why the inconcistency in the set? some are based according the reference, others are not?!
+#scan through all the refernce object in the scene
+    #go to its directory
+    #find the surfacing subdirectory of it
+    #import all the material from that surfacing directory
+    #then open accompanying json file .
+        #go through the list of object names and material
+        #apply all material accordingly
+        
+def SetMaterial(object, material):
     cmds.select(object, r = True )
     cmds.sets(e=True, forceElement = material);
-# load all  material
+        
+def getShaderPath(object):
+    if cmds.referenceQuery (object, inr = True):
+        Reference = pathlib.PurePath(cmds.referenceQuery(object ,filename=True)).parts
+        if ('model' in Reference):
+            
+            Reference = Reference [:Reference.index('model')] + ('surfacing', 'textures')
+            tmpPath = os.path.join(*Reference)
+            if os.path.exists(tmpPath) and (len(os.listdir(tmpPath)) > 0):
+                Reference  = Reference + (GetFileInPathWithVer( tmpPath,GetlatestVerInPath(tmpPath)),) 
+                return os.path.join(*Reference)
+            else:
+                return None      
+        else:
+            return None   
+    return None
+
+def GetContentFromJson(Path):
+    with open(Path, 'r') as f:
+          JsonTmp = json.load(f)
+    listr = list(JsonTmp.items())
+    return(listr)
+    
+LoadAllMaterial()
+
+#GetContentFromJson(r"C:\Users\janse\OneDrive\Documents\maya\projects\Assessment2_GroupX\scenes\publish\assets\prop\truck04\surfacing\textures\truck04_shaders_v002.json")
+#tmp = getSurfacingPath(cmds.ls(sl = True))
+#print (cmds.ls(sl = True))
+#print (tmp)
+#print(tmp +' contain '+str(os.listdir(tmp)) + " latest ver in directory " + str(GetlatestVerInPath(tmp)))
+#-------------------------------------------------------------------------------------------------HUD AND UI-----------------------------------------------------------------------------------------
+    
 
 
-#HUD AND UI
+#-------------------------------------------------------------------------------------------------Additional feature-----------------------------------------------------------------------------------------
 
-
-# Additional feature
-
-# individual loading of material based on version
+#individual loading of material based on version
 
